0fbede7b9b873bead7a95b59f1e7b3273a5d4fa1
Gunitsu <165042228+Gunitsu@users.noreply.github.com>
2024-04-21 21:15:22 -0300

    atualização

--- atividade8/js/controller.js
+++ atividade8/js/controller.js
@@ -1,71 +1,71 @@
 class WeatherController {
   constructor(service, view) {
-      this.weatherService = service;
-      this.weatherView = view;
-      this.searchBtn = document.querySelector("#search");
-      this.cityInput = document.querySelector("#city-input");
-      this.suggestionButtons = document.querySelectorAll("#suggestions button");
-      this.bindEvents();
+    this.weatherService = service;
+    this.weatherView = view;
+    this.searchBtn = document.querySelector("#search");
+    this.cityInput = document.querySelector("#city-input");
+    this.suggestionButtons = document.querySelectorAll("#suggestions button");
+    this.bindEvents();
   }
 
   bindEvents() {
     this.searchBtn.addEventListener("click", () => {
-        this.hideSuggestions(); // Oculta as sugestões ao clicar em "Pesquisar"
-        this.handleSearch();
+      this.hideSuggestions(); // Oculta as sugestões ao clicar em "Pesquisar"
+      this.handleSearch();
     });
     this.cityInput.addEventListener("keyup", (e) => {
-        if (e.code === "Enter") {
-            this.hideSuggestions(); // Oculta as sugestões ao pressionar Enter
-            this.handleSearch();
-        }
+      if (e.code === "Enter") {
+        this.hideSuggestions(); // Oculta as sugestões ao pressionar Enter
+        this.handleSearch();
+      }
     });
     this.suggestionButtons.forEach((btn) => {
-        btn.addEventListener("click", () => {
-            const city = btn.getAttribute("id");
-            this.hideSuggestions(); // Oculta as sugestões ao clicar em uma sugestão
-            this.showWeatherData(city);
-        });
+      btn.addEventListener("click", () => {
+        const city = btn.getAttribute("id");
+        this.hideSuggestions(); // Oculta as sugestões ao clicar em uma sugestão
+        this.showWeatherData(city);
+      });
     });
-}
+  }
 
-async handleSearch() {
-  const city = this.cityInput.value.trim();
-  if (city !== "") {
+  async handleSearch() {
+    const city = this.cityInput.value.trim();
+    if (city !== "") {
       this.weatherView.hideInformation(); // Oculta as informações antes de iniciar uma nova pesquisa
       this.weatherView.toggleLoader(true); // Exibe o loader
       await this.showWeatherData(city); // Realiza a pesquisa
       this.weatherView.toggleLoader(false); // Oculta o loader após a pesquisa
+    }
+  }
+
+  hideSuggestions() {
+    this.suggestionButtons.forEach((btn) => {
+      btn.style.display = "none"; // Oculta cada botão de sugestão
+    });
+  }
+
+  async showWeatherData(city) {
+    this.weatherView.toggleLoader();
+    const data = await this.weatherService.getWeatherData(city);
+    if (data.cod === "404") {
+      this.weatherView.showErrorMessage();
+      this.weatherView.toggleLoader();
+      return;
+    }
+    const weatherData = new WeatherData(
+      data.name,
+      parseInt(data.main.temp),
+      data.weather[0].description,
+      data.weather[0].icon,
+      data.sys.country,
+      data.main.humidity,
+      data.wind.speed
+    );
+    this.weatherView.showWeatherData(weatherData);
+    this.weatherView.toggleLoader();
   }
 }
 
-hideSuggestions() {
-    this.suggestionButtons.forEach((btn) => {
-        btn.style.display = "none"; // Oculta cada botão de sugestão
-    });
-}
-  
-    async showWeatherData(city) {
-      this.weatherView.toggleLoader();
-      const data = await this.weatherService.getWeatherData(city);
-      if (data.cod === "404") {
-        this.weatherView.showErrorMessage();
-        this.weatherView.toggleLoader();
-        return;
-      }
-      const weatherData = new WeatherData(
-        data.name,
-        parseInt(data.main.temp),
-        data.weather[0].description,
-        data.weather[0].icon,
-        data.sys.country,
-        data.main.humidity,
-        data.wind.speed
-      );
-      this.weatherView.showWeatherData(weatherData);
-      this.weatherView.toggleLoader();
-    }
-  }
-  
 // Configuração
 const apiKey = "5ce165099db98eb1a4172c9b8eea4597";
 const weatherService = new WeatherService(apiKey);
--- atividade8/js/view.js
+++ atividade8/js/view.js
@@ -1,47 +1,47 @@
 class WeatherView {
-  constructor() {
-      this.cityElement = document.querySelector("#city");
-      this.tempElement = document.querySelector("#temperature span");
-      this.descElement = document.querySelector("#description");
-      this.weatherIconElement = document.querySelector("#weather-icon");
-      this.countryElement = document.querySelector("#country");
-      this.umidityElement = document.querySelector("#umidity span");
-      this.windElement = document.querySelector("#wind span");
-      this.weatherContainer = document.querySelector("#weather-data");
-      this.errorMessageContainer = document.querySelector("#error-message");
-      this.loader = document.querySelector("#loader");
-  }
-
-  toggleLoader(showLoader) {
-    if (showLoader) {
-        this.loader.classList.remove("hide");
-    } else {
-        this.loader.classList.add("hide");
+    constructor() {
+        this.cityElement = document.querySelector("#city");
+        this.tempElement = document.querySelector("#temperature span");
+        this.descElement = document.querySelector("#description");
+        this.weatherIconElement = document.querySelector("#weather-icon");
+        this.countryElement = document.querySelector("#country");
+        this.umidityElement = document.querySelector("#umidity span");
+        this.windElement = document.querySelector("#wind span");
+        this.weatherContainer = document.querySelector("#weather-data");
+        this.errorMessageContainer = document.querySelector("#error-message");
+        this.loader = document.querySelector("#loader");
     }
-}
 
-  showErrorMessage() {
-      this.errorMessageContainer.classList.remove("hide");
-  }
+    toggleLoader(showLoader) {
+        if (showLoader) {
+            this.loader.classList.remove("hide");
+        } else {
+            this.loader.classList.add("hide");
+        }
+    }
 
-  hideInformation() {
-      this.errorMessageContainer.classList.add("hide");
-      this.weatherContainer.classList.add("hide");
-  }
+    showErrorMessage() {
+        this.errorMessageContainer.classList.remove("hide");
+    }
 
-  showWeatherData(weatherData) {
-    if (!weatherData.city || !weatherData.temperature || !weatherData.description || !weatherData.icon || !weatherData.country || !weatherData.humidity || !weatherData.windSpeed) {
+    hideInformation() {
+        this.errorMessageContainer.classList.add("hide");
+        this.weatherContainer.classList.add("hide");
+    }
+
+    showWeatherData(weatherData) {
+        if (!weatherData.city || !weatherData.temperature || !weatherData.description || !weatherData.icon || !weatherData.country || !weatherData.humidity || !weatherData.windSpeed) {
+            this.hideInformation();
+            return;
+        }
+
         this.hideInformation();
-        return;
+        this.cityElement.innerText = weatherData.city;
+        this.tempElement.innerText = parseInt(weatherData.temperature);
+        this.descElement.innerText = weatherData.description;
+        this.weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${weatherData.icon}.png`);
+        this.umidityElement.innerText = `${weatherData.humidity}%`;
+        this.windElement.innerText = `${weatherData.windSpeed}km/h`;
+        this.weatherContainer.classList.remove("hide");
     }
-
-    this.hideInformation();
-    this.cityElement.innerText = weatherData.city;
-    this.tempElement.innerText = parseInt(weatherData.temperature);
-    this.descElement.innerText = weatherData.description;
-    this.weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${weatherData.icon}.png`);
-    this.umidityElement.innerText = `${weatherData.humidity}%`;
-    this.windElement.innerText = `${weatherData.windSpeed}km/h`;
-    this.weatherContainer.classList.remove("hide");
-}
 }
